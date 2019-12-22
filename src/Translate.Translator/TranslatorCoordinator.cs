237b24938eb6cde1ce18b45097a10f5aa15d50c1
Riky Choudhuri <kalamos.github@outlook.com>
2019-12-22 11:09:52 +0000

    Generation of m3us with English before target lang

--- src/Translate.Translator/TranslatorCoordinator.cs
+++ src/Translate.Translator/TranslatorCoordinator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -10,6 +11,7 @@ namespace Translate.Translator
     public static class TranslatorCoordinator
     {
         private static Regex jsonRegex = new Regex("\"(?<h>[^\"]+)\"", RegexOptions.CultureInvariant | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private const string rootDirectory = @"C:\Users\Riky\Documents\Code\Translate\Translations";
 
         public static void Download(Conversation[] conversations, Language[] languages)
         {
@@ -17,81 +19,109 @@ namespace Translate.Translator
                 foreach (var conversation in conversations)
                 {
                     int phraseCounter = 1;
+                    var m3uBuilder = new List<FileInfo>();
                     foreach (var phrase in conversation.Phrases)
                     {
-                        var encodedPhrase = HttpUtility.UrlEncode(phrase);
-                        var translateUri = string.Format(Uris.TranslateApi, language.Code, encodedPhrase);
                         var paddedPhraseCounter = phraseCounter.ToString().PadLeft(3, '0');
-                        var jsonFileInfo = new FileInfo(
-                            @$"C:\Users\Riky\Documents\Code\Translate\Translations\{conversation.Name}\{language.Code}\{paddedPhraseCounter}_{language.Code}.json");
-                        if (!jsonFileInfo.Exists)
-                        {
-                            var r = WebRequest.Create(translateUri);
-                            var response = r.GetResponse();
 
-                            using (var stream = response.GetResponseStream())
-                            {
-                                jsonFileInfo.Directory.Create();
-                                using (var fs = new FileStream(jsonFileInfo.FullName, FileMode.Create))
-                                {
-                                    stream.CopyTo(fs);
-                                }
-                            }
-                            Thread.Sleep(5000);
-                        }
-                        phraseCounter++;
-
-                        var translatedLines = File.ReadAllLines(jsonFileInfo.FullName);
-
-                        var translatedPhrases = new List<string>();
-                        var matches = jsonRegex.Matches(translatedLines[0]);
-                        foreach (Match match in matches)
-                        {
-                            foreach (Group group in match.Groups)
-                            {
-                                if (group.Name == "0")
-                                {
-                                    continue;
-                                }
-                                foreach (Capture capture in group.Captures)
-                                {
-                                    translatedPhrases.Add(capture.Value);
-                                    if (translatedPhrases.Count == 2)
-                                    {
-                                        break;
-                                    }
-                                }
-                            }
-                        }
+                        var translatedLines = GetTranslatedPhrase(language, conversation, phrase, paddedPhraseCounter);
+                        var translatedPhrases = MatchTranslatedPhrases(translatedLines);
 
                         int c = 0;
                         foreach (var translatedPhrase in translatedPhrases)
                         {
-                            var lang = (c == 0) ? language.Code : "en";
+                            var lang = (c == 0) ? "en" : language.Code;
                             var urlEncodedTranslatedPhrase = HttpUtility.UrlEncode(translatedPhrase);
 
-                            var mp3FileInfo = new FileInfo(
-                                @$"C:\Users\Riky\Documents\Code\Translate\Translations\{conversation.Name}\{language.Code}\{paddedPhraseCounter}_{lang}.mp3");
-                            if (!mp3FileInfo.Exists)
-                            {
-                                var voiceUri = string.Format(Uris.VoiceApi, urlEncodedTranslatedPhrase, lang, lang.Length);
-                                var r = WebRequest.Create(voiceUri);
-                                var response = r.GetResponse();
-
-                                using (var stream = response.GetResponseStream())
-                                {
-                                    mp3FileInfo.Directory.Create();
-                                    using (var fs = new FileStream(mp3FileInfo.FullName, FileMode.Create))
-                                    {
-                                        stream.CopyTo(fs);
-                                    }
-                                }
-                                Thread.Sleep(5000);
-                            }
+                            var mp3Info = GetAudio(language, conversation, paddedPhraseCounter, lang, urlEncodedTranslatedPhrase);
+                            m3uBuilder.Add(mp3Info);
                             c++;
+                        }
+                        phraseCounter++;
+                    }
+                    var m3uFileInfo = new FileInfo(
+                        @$"{rootDirectory}\{conversation.Name}\{language.Code}\{conversation.Name}_{language.Code}.m3u");
+                    if (!m3uFileInfo.Exists)
+                    {
+                        m3uFileInfo.Directory.Create();
+                        File.WriteAllLines(m3uFileInfo.FullName, m3uBuilder.Select(x => x.FullName).ToArray());
+                    }
+                }
+        }
+
+        private static FileInfo GetAudio(Language language, Conversation conversation, string paddedPhraseCounter, string lang, string urlEncodedTranslatedPhrase)
+        {
+            var mp3FileInfo = new FileInfo(
+                @$"{rootDirectory}\{conversation.Name}\{language.Code}\{paddedPhraseCounter}_{lang}.mp3");
+            if (!mp3FileInfo.Exists)
+            {
+                var voiceUri = string.Format(Uris.VoiceApi, urlEncodedTranslatedPhrase, lang, lang.Length);
+                var request = WebRequest.Create(voiceUri);
+                var response = request.GetResponse();
+
+                using (var stream = response.GetResponseStream())
+                {
+                    mp3FileInfo.Directory.Create();
+                    using (var fs = new FileStream(mp3FileInfo.FullName, FileMode.Create))
+                    {
+                        stream.CopyTo(fs);
+                    }
+                }
+                Thread.Sleep(5000);
+            }
+            return mp3FileInfo;
+        }
+
+        private static List<string> MatchTranslatedPhrases(string[] translatedLines)
+        {
+            var translatedPhrases = new List<string>();
+            var matches = jsonRegex.Matches(translatedLines[0]);
+            foreach (Match match in matches)
+            {
+                foreach (Group group in match.Groups)
+                {
+                    if (group.Name == "0")
+                    {
+                        continue;
+                    }
+                    foreach (Capture capture in group.Captures)
+                    {
+                        translatedPhrases.Add(capture.Value);
+                        if (translatedPhrases.Count == 2)
+                        {
+                            translatedPhrases.Reverse(); // make english first
+                            return translatedPhrases;
                         }
                     }
                 }
+            }
+            throw new System.Exception($"Not enough phrases found in {translatedLines[0]}");
+        }
+
+        private static string[] GetTranslatedPhrase(Language language, Conversation conversation, string phrase, string paddedPhraseCounter)
+        {
+            var encodedPhrase = HttpUtility.UrlEncode(phrase);
+            var translateUri = string.Format(Uris.TranslateApi, language.Code, encodedPhrase);
+            var jsonFileInfo = new FileInfo(
+                @$"{rootDirectory}\{conversation.Name}\{language.Code}\{paddedPhraseCounter}_{language.Code}.json");
+            if (!jsonFileInfo.Exists)
+            {
+                var request = WebRequest.Create(translateUri);
+                var response = request.GetResponse();
+
+                using (var stream = response.GetResponseStream())
+                {
+                    jsonFileInfo.Directory.Create();
+                    using (var fs = new FileStream(jsonFileInfo.FullName, FileMode.Create))
+                    {
+                        stream.CopyTo(fs);
+                    }
+                }
+                Thread.Sleep(5000);
+            }
+
+            var translatedLines = File.ReadAllLines(jsonFileInfo.FullName);
+            return translatedLines;
         }
     }
 }
--- src/Translate.Translator/Uris.cs
+++ src/Translate.Translator/Uris.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Translate.Translator
+﻿namespace Translate.Translator
 {
     public static class Uris
     {
         public const string VoiceApi = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q={0}&tl={1}&total=1&idx=0&textlen={2}";
-        public const string TranslateApi =  "https://translate.googleapis.com/translate_a/single?client=gtx&sl=EN&tl={0}&dt=t&q={1}";
+        public const string TranslateApi = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=EN&tl={0}&dt=t&q={1}";
     }
 }
